01cc09c61b6325b6f627e7464c4cb49e84fd1a58
terumiisobe <isobe.terumi@hotmail.com>
2019-09-10 19:54:03 -0300

    list method implemented

--- Benchmark X/bookstore/src/main/java/dao/ItemDao.java
+++ Benchmark X/bookstore/src/main/java/dao/ItemDao.java
@@ -1,5 +1,48 @@
 package dao;
 
-public class ItemDao {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import model.Item;
+
+@Stateful
+public class ItemDao {
+	
+	private EntityManager em;
+
+	public List<Item> list(String title, String author, Boolean sorted, Long maxPrice){		
+		StringBuilder sb = new StringBuilder();
+		Map<String, Object> parameters = new HashMap<>();
+		
+		sb.append("select i from Item i ");
+		sb.append("where 1=1 ");
+		
+		if(title != null) {
+			sb.append("and i.title like :title ");
+			parameters.put("title", title);
+		}
+		
+		if(author != null) {
+			sb.append("and i.author like :author ");
+			parameters.put("author", author);
+		}
+		
+		if(maxPrice != null) {
+			sb.append("and i.cost <= :maxPrice ");
+			parameters.put("maxPrice", maxPrice);
+		}
+		
+		if(sorted != null) {
+			sb.append("order by i.cost ");
+		}
+		
+		TypedQuery<Item> query = em.createQuery(sb.toString(), Item.class);
+		return query.getResultList();
+		
+	}
 }
--- Benchmark X/bookstore/src/main/java/rn/ItemRn.java
+++ Benchmark X/bookstore/src/main/java/rn/ItemRn.java
@@ -1,5 +1,36 @@
 package rn;
 
-public class ItemRn {
+import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.inject.Inject;
+
+import api.ItemApi;
+import dao.ItemDao;
+import model.Item;
+
+public class ItemRn {
+	
+	@Inject 
+	ItemDao itemDao;
+	
+	/**
+	 * Lists products by title, author, sorted by cheapest price and maximum price.
+	 */
+	public List<ItemApi> list(String title, String author, Boolean sorted, Long maxPrice){	
+		List<Item> list = itemDao.list(title, author, sorted, maxPrice);
+		List<ItemApi> listApi = list.stream().map(i -> convertToApi(i)).collect(Collectors.toList());
+	}
+	
+	private ItemApi convertToApi(Item item) {
+		ItemApi api = new ItemApi();
+		api.setId(item.getId());
+		api.setTitle(item.getTitle());
+		api.setAuthor(item.getAuthor().getFullName());
+		api.setPublisher(item.getPublisher());
+		api.setCost(item.getCost());
+		api.setAvailability(item.getAvailability());
+		
+		return api;
+	}
 }
