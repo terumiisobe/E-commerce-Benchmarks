5df575583da366d0be2a37b2199e7babaa0672cb
terumiisobe <isobe.terumi@hotmail.com>
2019-09-12 21:02:02 -0300

    customer searchById methos

---
+++ Benchmark X/bookstore/src/main/java/dao/CustomerDao.java
@@ -0,0 +1,30 @@
+package dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import model.Customer;
+
+public class CustomerDao {
+	
+	private EntityManager em;
+	
+	public Customer searchById(Long id) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("select c from Customer ");
+		sb.append("where c.id = :id ");
+		TypedQuery<Customer> query = em.createQuery(sb.toString(), Customer.class);
+		query.setParameter("id", id);
+		return query.getSingleResult();
+	}
+	
+	public Customer fetchCustomer(String username) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("select c from Customer ");
+		sb.append("where c.username = :username ");
+		TypedQuery<Customer> query = em.createQuery(sb.toString(), Customer.class);
+		query.setParameter("username", username);
+		return query.getSingleResult();
+	}
+
+}
