304490f46269906ed078d764e32bf32997d655cc
terumiisobe <isobe.terumi@hotmail.com>
2019-09-28 12:13:20 -0300

    insert sql

--- Benchmark X/bookstore/src/main/resources/import.sql
+++ Benchmark X/bookstore/src/main/resources/import.sql
@@ -2,3 +2,17 @@
 -- Reconstruction
 --INSERT INTO UltrasoundImage (userId, fileReference, reconstructionStartTime, reconstructionEndTime, size, iterationsPerformed) VALUES (1, 'filename', CURRENT_DATE, CURRENT_DATE, 1024, 50);
 --INSERT INTO User (userId, username) VALUES (1, 'fenix');
+
+--INSERT INTO Item(id, title, author, publisher, cost, availability, timesSold)
+--VALUES (1L, 'The Name of the Wind', 'Patrick Rothfuss', 'Sextante', 80, 150L, 0L);
+--VALUES (2L, 'The Game of Thrones', 'George R. R. Martin', 'Sextante', 90, 200L, 0L);
+--VALUES (3L, 'Lord of the Rings', 'J. R. R. Tolkien', 'Sextante', 50, 130L, 0L);
+
+
+INSERT INTO Author
+(id, fullName)
+VALUES(1L, 'Patrick Rothfuss');
+
+INSERT INTO Item
+(id, availability, cost, publisher, timesSold, title, authorId)
+VALUES(1L, 150L, 80, 'Sextante', 0L, 'The Name of the Wind', 1L);
