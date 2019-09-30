e7a289ffe2139b359c5bb7a78d90a3b1af19abb6
terumiisobe <isobe.terumi@hotmail.com>
2019-09-30 20:55:50 -0300

    insert fields subject and publication date

--- Benchmark X/bookstore/src/main/java/api/ItemApi.java
+++ Benchmark X/bookstore/src/main/java/api/ItemApi.java
@@ -1,13 +1,16 @@
 package api;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 public class ItemApi {
 	
 	private Long id;
 	private String title;
 	private String author;
+	private String subject;
 	private String publisher;
+	private Date publicationDate;
 	private BigDecimal cost;
 	private Long availability;
 	
@@ -29,11 +32,23 @@ public class ItemApi {
 	public void setAuthor(String author) {
 		this.author = author;
 	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
 	public String getPublisher() {
 		return publisher;
 	}
 	public void setPublisher(String publisher) {
 		this.publisher = publisher;
+	}
+	public Date getPublicationDate() {
+		return publicationDate;
+	}
+	public void setPublicationDate(Date publicationDate) {
+		this.publicationDate = publicationDate;
 	}
 	public BigDecimal getCost() {
 		return cost;
