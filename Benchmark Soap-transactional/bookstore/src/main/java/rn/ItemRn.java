484c43573da2608f6d8ab81618dfd5a509864afd
terumiisobe <isobe.terumi@hotmail.com>
2020-02-27 21:33:43 -0400

    feat(Item): inserir logs para analise

--- Benchmark Soap-transactional/bookstore/src/main/java/rn/ItemRn.java
+++ Benchmark Soap-transactional/bookstore/src/main/java/rn/ItemRn.java
@@ -29,7 +29,8 @@ public class ItemRn {
 	/**
 	 * Searches for item that match the parameters up to 50 items.
 	 */
-	public List<ItemApi> search(EnumSearchType searchType, String searchText){	
+	public List<ItemApi> search(EnumSearchType searchType, String searchText){
+		System.out.println("------search");
 		List<Item> list = itemDao.search(searchType, searchText);
 		List<ItemApi> listApi = list.stream().map(i -> convertToApi(i)).collect(Collectors.toList());
 		return listApi;
@@ -39,6 +40,7 @@ public class ItemRn {
 	 * Lists the first 50 items from a subject sorted by ascending title.  
 	 */
 	public List<ItemApi> bestSellers(String subject){	
+		System.out.println("------best sellers");
 		List<Item> list = itemDao.bestSellers(subject);
 		List<ItemApi> listApi = list.stream().map(i -> convertToApi(i)).collect(Collectors.toList());		
 		return listApi;
@@ -47,7 +49,8 @@ public class ItemRn {
 	/**
 	 * Lists the first 50 items from a subject sorted by descending publication date and ascending title.  
 	 */
-	public List<ItemApi> newProducts(String subject){	
+	public List<ItemApi> newProducts(String subject){		
+		System.out.println("------new products");
 		List<Item> list = itemDao.newProducts(subject);
 		List<ItemApi> listApi = list.stream().map(i -> convertToApi(i)).collect(Collectors.toList());
 		return listApi;
@@ -57,6 +60,7 @@ public class ItemRn {
 	 * Lists all subjects for best sellers and new items, and the name of the customer if registered.
 	 */
 	public HomeApi home(Long token){
+		System.out.println("------home");
 		HomeApi api = new HomeApi();
 		ShoppingSession session = null;
 		if(token != null) {
@@ -75,7 +79,8 @@ public class ItemRn {
 	/**
 	 * Search for an item detail.
 	 */
-	public ItemApi productDetail(Long id) {
+	public ItemApi productDetail(Long id) {	
+		System.out.println("------product detail");
 		Item item = itemDao.productDetail(id);
 		return convertToApi(item);
 	}
